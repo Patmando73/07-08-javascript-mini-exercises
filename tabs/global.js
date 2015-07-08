84879b3764e5cb0fce20d478c3577b0af70f31d1
Patrick Carlton <pcarlton73@gmail.com>
2015-07-08 11:25:42 -0500

    tabs exercise finished! :)

--- tabs/global.js
+++ tabs/global.js
@@ -1,3 +1,32 @@
 /**
  * Created by Patrick Carlton & Lars Hollingsed on 7/8/15.
  */
+
+
+function show_info1() {
+    document.getElementById("tab1_info").style.visibility="visible";
+    document.getElementById("tab2_info").style.visibility="hidden";
+    document.getElementById("tab3_info").style.visibility="hidden";
+
+}
+
+function show_info2() {
+    document.getElementById("tab2_info").style.visibility="visible";
+    document.getElementById("tab3_info").style.visibility="hidden";
+    document.getElementById("tab1_info").style.visibility="hidden";
+
+}
+
+function show_info3() {
+    document.getElementById("tab3_info").style.visibility="visible";
+    document.getElementById("tab2_info").style.visibility="hidden";
+    document.getElementById("tab1_info").style.visibility="hidden";
+}
+
+show_info1();
+
+document.getElementById("tab2_button").onclick = show_info2;
+
+document.getElementById("tab3_button").onclick = show_info3;
+
+document.getElementById("tab1_button").onclick = show_info1;
