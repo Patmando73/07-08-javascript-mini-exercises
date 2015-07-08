f67bae5257d41ccadaa0eccfc4f0fa345ea806f7
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-08 12:37:43 -0500

    Barcamp Hidden Menu + Shadow

--- barcamp-mobile-nav/global.js
+++ barcamp-mobile-nav/global.js
@@ -1,3 +1,19 @@
 /**
  * Created by Patrick Carlton & Lars Hollingsed on 7/8/15.
  */
+
+function showHiddenMenu() {
+  document.getElementById("hidden_menu").style.top = "0px";
+  document.getElementById("show_menu_button").style.visibility = "hidden";
+  document.getElementById("hide_menu_button").style.visibility = "visible";
+}
+
+function hideHiddenMenu() {
+  document.getElementById("hidden_menu").style.top = "-300px";
+  document.getElementById("hide_menu_button").style.visibility = "hidden";
+  document.getElementById("show_menu_button").style.visibility = "visible";
+}
+
+
+document.getElementById("show_menu_button").onclick = showHiddenMenu;
+document.getElementById("hide_menu_button").onclick = hideHiddenMenu;
